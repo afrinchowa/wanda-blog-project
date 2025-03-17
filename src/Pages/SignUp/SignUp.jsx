5d7a451dd5c799b918dd081d92bf1c1f7a6baba8
chowa <arfinchowa524@gmail.com>
2025-03-18 00:19:00 +0600

    sign up page done by arif

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,11 +1,80 @@
-
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const SignUp = () => {
-    return (
-        <div>
-            
+  const { createUser } = useContext(AuthContext);
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("form sign up", email, password, name);
+    createUser(email, password)
+      .then((result) => {
+        console.log('user created at fb',result.user);
+        const createdAt = result?.user?.metadata?.creationTime;
+        const newUser = { name, email ,createdAt};
+        // save new user info to the database
+        fetch("http://localhost:5000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(newUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if(data.insertedId){
+                console.log("user created to db");
+            }
+           
+          });
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleSignUp} className="card-body">
+              <fieldset className="fieldset">
+                <label className="fieldset-label">Name</label>
+                <input
+                  type="name"
+                  className="input"
+                  placeholder="Name"
+                  name="name"
+                />
+                <label className="fieldset-label">Email</label>
+                <input
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                  name="email"
+                />
+                <label className="fieldset-label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/routes';
+import AuthProvider from './providers/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <div className="max-w-screen-xl mx-auto">
+   <AuthProvider >
    <RouterProvider router={router} />
+   </AuthProvider>
    </div>
   </StrictMode>,
 )
